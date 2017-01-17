f769ab683f83b41a7833c5e503f3b951f5fb44bb
github username <github email address>
2017-01-17 15:29:53 -0500

    all green

--- lib/format_minutes.rb
+++ lib/format_minutes.rb
@@ -1,3 +1,9 @@
 def time_formatter(minutes)
-  # your code here
+  str = ""
+  hours = minutes / 60 if minutes >= 60
+  minutes -= (hours * 60) if hours
+  str << "#{hours} hour(s)" if hours
+  str << ", #{minutes} minute(s)" if hours && minutes != 0
+  str << "#{minutes} minute(s)" if !hours
+  str
 end
